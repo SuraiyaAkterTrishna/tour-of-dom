5be3dd51c7e711e871848cd46639407a02441c75
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-21 03:35:06 +0600

    innerText, innerHTML, appendChild

--- dom3.js
+++ dom3.js
@@ -1,3 +1,41 @@
+// food section create dynamically 
+const main = document.getElementById('main-container');
+
+const newSection = document.createElement('section');
+
+const h1 = document.createElement('h1');
+h1.innerText = 'My Food Gallery';
+newSection.appendChild(h1);
+
+const ul = document.createElement('ul');
+
+const li1 = document.createElement('li');
+li1.innerText = 'Beriany';
+ul.appendChild(li1);
+
+const li2 = document.createElement('li');
+li2.innerText = 'Borhani';
+ul.appendChild(li2);
+
+const li3 = document.createElement('li');
+li3.innerText = 'Sandwitch';
+ul.appendChild(li3);
+
+newSection.appendChild(ul);
+main.appendChild(newSection);
+
+// dress section create dynamically 
+let section = document.createElement('section');
+section.innerHTML = `
+<h1>My Dress section</h1>
+<ul>
+    <li>T-shirt</li>
+    <li>Pant</li>
+    <li>Panjabi</li>
+</ul>
+`;
+main.appendChild(section);
+
 let allSection = document.querySelectorAll('section');
 for (const section of allSection) {
     section.style.backgroundColor = 'lavender';
@@ -8,6 +46,11 @@ let dom = document.getElementById('dom');
 dom.classList.add('center');
 dom.classList.add('bold');
 dom.classList.remove('center');
+
+const places = document.getElementById('place-ul');
+const li = document.createElement('li');
+li.innerText = 'PaharToli';
+places.appendChild(li);
 
 
 
