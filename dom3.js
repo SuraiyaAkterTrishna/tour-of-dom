6f0d67159b5236a1f99d78e8c990caa161508391
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-21 01:04:03 +0600

    adding css style with javascript

---
+++ dom3.js
@@ -0,0 +1,8 @@
+let allSection = document.querySelectorAll('section');
+for (const section of allSection) {
+    section.style.backgroundColor = 'lavender';
+    section.style.margin = '10px 40px';
+    section.style.padding = '20px';
+}
+
+
