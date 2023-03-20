fcc3573676202479554655c51dbe791a47748eca
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-21 01:11:47 +0600

    classList, add class, remove class

--- dom3.js
+++ dom3.js
@@ -4,5 +4,10 @@ for (const section of allSection) {
     section.style.margin = '10px 40px';
     section.style.padding = '20px';
 }
+let dom = document.getElementById('dom');
+dom.classList.add('center');
+dom.classList.add('bold');
+dom.classList.remove('center');
+
 
 
